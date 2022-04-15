cbed8587fb436d97c7fd6655f686d38adb84f773
Binary <58886994+S-quark1@users.noreply.github.com>
2022-04-15 09:40:59 +0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,79 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
-import 'package:tadam/Info.dart';
+import 'package:get_movie/views/search_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Node server demo',
       debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(
-        splash: Lottie.asset(
-          'assets/image/97111-loading-spinner-dots.json',
-        ),
-        splashIconSize: 300,
-        nextScreen: Info(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
       ),
+      home: const SearchPage(),
+      // home: Scaffold(
+      //   appBar: AppBar(title: Text('Flutter Client')),
+      //   body: BodyWidget(),
+      // ),
     );
   }
 }
+
+// class BodyWidget extends StatefulWidget {
+//   @override
+//   BodyWidgetState createState() {
+//     return new BodyWidgetState();
+//   }
+// }
+//
+// class BodyWidgetState extends State<BodyWidget> {
+//   String serverResponse = 'Server response';
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return Padding(
+//       padding: const EdgeInsets.all(32.0),
+//       child: Align(
+//         alignment: Alignment.topCenter,
+//         child: SizedBox(
+//           width: 200,
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: <Widget>[
+//               ElevatedButton(
+//                 child: Text('Send request to server'),
+//                 onPressed: () {
+//                   _makeGetRequest();
+//                 },
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.all(8.0),
+//                 child: Text(serverResponse),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+//
+//   _makeGetRequest() async {
+//     final url = Uri.parse(_localhost());
+//     Response response = await get(url);
+//     setState(() {
+//       serverResponse = response.body;
+//     });
+//   }
+//
+//   String _localhost() {
+//     if (Platform.isAndroid)
+//       return 'http://10.0.2.2:3000';
+//     else // for iOS simulator
+//       return 'http://localhost:3000';
+//   }
+// }
